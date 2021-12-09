ee43b875d4c2a6b43a122f932f1418358e31fcbd
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-09 16:37:34 +0200

    Hangman 3-rd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,8 +1,12 @@
+import random
+
 print("ВИСЕЛИЦА")
 print("Игра скоро будет доступна")
+list_version = ['python', 'java', 'javascript', 'php']
+random_1 = random.choice(list_version)
 print("Угадай слово:")
 result_1 = input()
-if result_1 == 'python':
+if result_1 == random_1:
     print("Вы выжили!")
 else:
     print("Вы проиграли!")
