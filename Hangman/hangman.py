833a633cb0aa34af330136eb912af4b369df22e5
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-09 16:23:05 +0200

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,2 +1,8 @@
 print("ВИСЕЛИЦА")
 print("Игра скоро будет доступна")
+print("Угадай слово:")
+result_1 = input()
+if result_1 == 'python':
+    print("Вы выжили!")
+else:
+    print("Вы проиграли!")
