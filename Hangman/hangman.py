6ea752d7fd9b696f84bf0549eedb4dccbcee0b61
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-16 13:06:51 +0200

    Hangman 4.1-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,8 +4,9 @@ print("ВИСЕЛИЦА")
 print("Игра скоро будет доступна")
 list_version = ['python', 'java', 'javascript', 'php']
 random_1 = random.choice(list_version)
-example_1 = random_1[0] + random_1[1] + random_1[2] + '-' * (len(random_1) - 3)
-print("Угадай слово " + example_1 + ":")
+random_2 = list(len(list(random_1)) * "-")
+
+print("Угадай слово:")
 result_1 = input()
 if result_1 == random_1:
     print("Вы выжили!")
