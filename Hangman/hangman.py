c30c9308f41c4c3254fae21a397fbc8799b5154d
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-18 23:10:36 +0200

    Hangman 5-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,11 +4,20 @@ print("ВИСЕЛИЦА")
 print("Игра скоро будет доступна")
 list_version = ['python', 'java', 'javascript', 'php']
 random_1 = random.choice(list_version)
-random_2 = list(len(list(random_1)) * "-")
-
+random_2 = list(random_1)
+random_3 = list(len(random_1) * "-")
 print("Угадай слово:")
-result_1 = input()
-if result_1 == random_1:
-    print("Вы выжили!")
-else:
-    print("Вы проиграли!")
+for i in range(10):
+    result_1 = str(input("Введите одну из букв:"))
+    if result_1 in random_2:
+        if random_2.count(result_1) >= 2:
+            ind = random_2.index(result_1)
+            random_3[ind] = result_1
+            random_2[ind] = "-"
+        ind = random_2.index(result_1)
+        random_3[ind] = result_1
+    else:
+        print("В слове такой буквы нет, попробуйте снова!")
+    print(''.join(random_3))
+print("Спасибо за игру!")
+print("Посмотрим, насколько хорошо вы справились на следующем этапе)")
