57641f22aff3b9eae433dd2c4bd4b1a0c7ea85a3
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-21 14:16:05 +0200

    CoffeeMachine 3-rd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -5,9 +5,20 @@ print("Начинаем варить кофе\n"
       "Налив кофе в чашку\n"
       "Налить немного молока в чашку\n"
       "Кофе готов!")
+count_water = int(input("Напишите, сколько мл воды в кофемашине:"))
+count_milk = int(input("Напишите, сколько мл молока в кофемашине:"))
+count_coffee_beans = int(input("Напишите, сколько кофейных зерен в кофемашине:"))
 number = int(input("Напишите, сколько чашек кофе вам понадобится:"))
 water = number * 200
 milk = number * 50
 coffee_beans = number * 15
+if water * 2 <= count_water and milk * 2 <= count_milk and coffee_beans * 2 <= count_coffee_beans:
+      N = round(min(count_water / 200, count_milk / 50, count_coffee_beans / 15) - number)
+      print("Да, я могу приготовить такое количество кофе (даже больше на", N, "больше)")
+elif count_water >= water and count_milk >= milk and count_coffee_beans >= coffee_beans:
+      print("Да, я могу приготовить такое количество кофе")
+else:
+      N = round(min(count_water / 200, count_milk / 50, count_coffee_beans / 15))
+      print("Нет, я могу приготовить только", N, "чашек кофе")
 print("На", number, "чашек кофе понядобится:\n" + str(water) + " мл воды\n" + str(milk) + " мл молока\n" +
       str(coffee_beans) + " кофейных зерен")
