841e86ce74d20daf1b981991d1bd71b22c8842fa
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-22 18:11:42 +0200

    CoffeeMachine 5-th stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -5,7 +5,7 @@ count_cups = 9
 count_money = 550
 
 
-def info():
+def remaining():
     print("В кофе машине есть:\n",
           str(count_water), "воды\n",
           str(count_milk), "молока\n",
@@ -14,58 +14,75 @@ def info():
           str(count_money), "денег\n")
 
 
-info()
-
-
 def start():
-    choice_user = input("Напишите действие (buy - купить, fill - заполнить, take - взять):")
+    choice_user = input(
+        "Напишите действие (buy - купить, fill - заполнить, take - взять, remaining - остаток, exit - выход):")
     if choice_user == "buy":
         buy()
-        info()
     elif choice_user == "fill":
         fill()
-        info()
     elif choice_user == "take":
         take()
-        info()
+    elif choice_user == "remaining":
+        remaining()
+    elif choice_user == "exit":
+        return "exit"
+
+
+def enough():
+    global count_water, count_milk, count_coffee_beans, count_money
+    if count_water <= 0:
+        print("Извините, недостаточно воды")
+    elif count_milk <= 0:
+        print("Извините, недостаточно молока")
+    elif count_coffee_beans <= 0:
+        print("Извините, недостаточно кофейных зерен")
+    elif count_cups <= 0:
+        print("К сожалению, одноразовых стаканчиков недостаточно")
 
 
 def buy():
-    print("Что ты хочешь купить? 1 - эспрессо, 2 - латте, 3 - капучино:")
-    global count_water
-    global count_milk
-    global count_coffee_beans
-    global count_cups
-    global count_money
-    choice_user = int(input())
-    if choice_user == 1:
+    print("Что ты хочешь купить: 1 - эспрессо, 2 - латте, 3 - капучино, back - вернуться в главное меню")
+    global count_water, count_milk, count_coffee_beans, count_cups, count_money
+    choice_user = input()
+    if choice_user == str(1):
         count_water -= 250
         count_coffee_beans -= 16
         count_cups -= 1
         count_money += 4
-    elif choice_user == 2:
+        if enough() == None:
+            print("У меня достаточно ресурсов, чтобы приготовить тебе кофе!")
+        else:
+            print(enough())
+    elif choice_user == str(2):
         count_water -= 350
         count_milk -= 75
         count_coffee_beans -= 20
         count_cups -= 1
         count_money += 7
-    elif choice_user == 3:
+        if enough() == None:
+            print("У меня достаточно ресурсов, чтобы приготовить тебе кофе!")
+        else:
+            print(enough())
+    elif choice_user == str(3):
         count_water -= 200
         count_milk -= 100
         count_coffee_beans -= 12
         count_cups -= 1
         count_money += 6
+        if enough() == None:
+            print("У меня достаточно ресурсов, чтобы приготовить тебе кофе!")
+        else:
+            print(enough())
+    elif choice_user == "back":
+        return "back"
 
 
 def fill():
-    global count_water
-    global count_milk
-    global count_coffee_beans
-    global count_cups
-    global count_money
+    global count_water, count_milk, count_coffee_beans, count_cups, count_money
     result_water = int(input("Напишите, сколько мл воды вы хотите добавить:"))
     count_water += result_water
-    result_milk = int(input("Напишите, сколько мл молока вы хотите добавить"))
+    result_milk = int(input("Напишите, сколько мл молока вы хотите добавить:"))
     count_milk += result_milk
     result_coffee_beans = int(input("Напишите, сколько граммов кофейных зерен вы хотите добавить:"))
     count_coffee_beans += result_coffee_beans
@@ -79,4 +96,6 @@ def take():
     count_money = 0
 
 
-start()
+facts_start = 0
+while facts_start != "exit":
+    facts_start = start()
