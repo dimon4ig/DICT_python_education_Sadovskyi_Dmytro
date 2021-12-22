5a2d26e853c6bfad65c5d1c262b47673131e9623
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-22 03:36:05 +0200

    CoffeeMachine 4-th stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -1,24 +1,82 @@
-print("Начинаем варить кофе\n"
-      "Измельчение кофейных зерен\n"
-      "Кипящая вода\n"
-      "Смешивание кипяченой воды с измельченными кофейными зернами\n"
-      "Налив кофе в чашку\n"
-      "Налить немного молока в чашку\n"
-      "Кофе готов!")
-count_water = int(input("Напишите, сколько мл воды в кофемашине:"))
-count_milk = int(input("Напишите, сколько мл молока в кофемашине:"))
-count_coffee_beans = int(input("Напишите, сколько кофейных зерен в кофемашине:"))
-number = int(input("Напишите, сколько чашек кофе вам понадобится:"))
-water = number * 200
-milk = number * 50
-coffee_beans = number * 15
-if water + 200 <= count_water and milk + 50 <= count_milk and coffee_beans + 15 <= count_coffee_beans:
-    N = min(count_water // 200, count_milk // 50, count_coffee_beans // 15) - number
-    print("Да, я могу приготовить такое количество кофе (даже на", N, "чашек больше)")
-elif count_water >= water and count_milk >= milk and count_coffee_beans >= coffee_beans:
-    print("Да, я могу приготовить такое количество кофе")
-else:
-    N = min(count_water // 200, count_milk // 50, count_coffee_beans // 15)
-    print("Нет, я могу приготовить только", N, "чашек кофе")
-print("На", number, "чашек кофе понядобится:\n" + str(water) + " мл воды\n" + str(milk) + " мл молока\n" +
-      str(coffee_beans) + " кофейных зерен")
+count_water = 400
+count_milk = 540
+count_coffee_beans = 120
+count_cups = 9
+count_money = 550
+
+
+def info():
+    print("В кофе машине есть:\n",
+          str(count_water), "воды\n",
+          str(count_milk), "молока\n",
+          str(count_coffee_beans), "зерен кофе\n",
+          str(count_cups), "одноразовых стаканчиков\n",
+          str(count_money), "денег\n")
+
+
+info()
+
+
+def start():
+    choice_user = input("Напишите действие (buy - купить, fill - заполнить, take - взять):")
+    if choice_user == "buy":
+        buy()
+        info()
+    elif choice_user == "fill":
+        fill()
+        info()
+    elif choice_user == "take":
+        take()
+        info()
+
+
+def buy():
+    print("Что ты хочешь купить? 1 - эспрессо, 2 - латте, 3 - капучино:")
+    global count_water
+    global count_milk
+    global count_coffee_beans
+    global count_cups
+    global count_money
+    choice_user = int(input())
+    if choice_user == 1:
+        count_water -= 250
+        count_coffee_beans -= 16
+        count_cups -= 1
+        count_money += 4
+    elif choice_user == 2:
+        count_water -= 350
+        count_milk -= 75
+        count_coffee_beans -= 20
+        count_cups -= 1
+        count_money += 7
+    elif choice_user == 3:
+        count_water -= 200
+        count_milk -= 100
+        count_coffee_beans -= 12
+        count_cups -= 1
+        count_money += 6
+
+
+def fill():
+    global count_water
+    global count_milk
+    global count_coffee_beans
+    global count_cups
+    global count_money
+    result_water = int(input("Напишите, сколько мл воды вы хотите добавить:"))
+    count_water += result_water
+    result_milk = int(input("Напишите, сколько мл молока вы хотите добавить"))
+    count_milk += result_milk
+    result_coffee_beans = int(input("Напишите, сколько граммов кофейных зерен вы хотите добавить:"))
+    count_coffee_beans += result_coffee_beans
+    result_cups = int(input("Напишите, сколько одноразовых кофейных стаканчиков вы хотите добавить:"))
+    count_cups += result_cups
+
+
+def take():
+    global count_money
+    print("Вам выдали: " + str(count_money))
+    count_money = 0
+
+
+start()
