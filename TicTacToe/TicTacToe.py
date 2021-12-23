d73f8f07c2b0db89cf5e0334d066538f68b3fd29
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-23 02:35:24 +0200

    TicTacToe 4-th stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,4 +1,12 @@
 a = list(input("Введите ячейки:"))
+region = str('---------')
+s_1 = ['|', a[0], a[1], a[2], '|']
+s_2 = ['|', a[3], a[4], a[5], '|']
+s_3 = ['|', a[6], a[7], a[8], '|']
+dig = [int(i) for i in range(1, 4)]  # i хранящая в себе числа 1, 2 и 3
+s_l = [' ', s_1, s_2, s_3, ' ']
+answer_1 = 0
+answer_2 = 0
 
 
 def wins():
@@ -7,22 +15,22 @@ def wins():
         return "Невозможно"
     elif a.count("O") - a.count("X") >= 2:
         return "Невозможно"
-    if a[0] == a[1] == a[2] != "_":
-        res.append(a[0])
-    if a[3] == a[4] == a[5] != "_":
-        res.append(a[3])
-    if a[6] == a[7] == a[8] != "_":
-        res.append(a[6])
-    if a[0] == a[3] == a[6] != "_":
-        res.append(a[0])
-    if a[1] == a[4] == a[7] != "_":
-        res.append(a[1])
-    if a[2] == a[5] == a[8] != "_":
-        res.append(a[2])
-    if a[0] == a[4] == a[8] != "_":
-        res.append(a[0])
-    if a[2] == a[4] == a[6] != "_":
-        res.append(a[2])
+    if s_1[1] == s_1[2] == s_1[3] != "_":
+        res.append(s_1[1])
+    if s_2[1] == s_2[2] == s_2[3] != "_":
+        res.append(s_2[1])
+    if s_3[1] == s_3[2] == s_3[3] != "_":
+        res.append(s_3[6])
+    if s_1[1] == s_2[1] == s_3[1] != "_":
+        res.append(s_1[1])
+    if s_1[2] == s_2[2] == s_3[2] != "_":
+        res.append(s_1[2])
+    if s_1[3] == s_2[3] == s_3[3] != "_":
+        res.append(s_1[3])
+    if s_1[1] == s_2[2] == s_3[3] != "_":
+        res.append(s_1[1])
+    if s_1[3] == s_2[2] == s_3[1] != "_":
+        res.append(s_1[3])
     if res.count("X") >= 2:
         res.remove("X")
     elif res.count("O") >= 2:
@@ -40,12 +48,36 @@ def wins():
 
 
 def square():
-    print("---------------\n" +
-          "|", a[0], "  ", a[1], "  ", a[2], "|\n" +
-          "|", a[3], "  ", a[4], "  ", a[5], "|\n" +
-          "|", a[6], "  ", a[7], "  ", a[8], "|\n" +
-          "---------------")
+    print("-------------\n" +
+          '  '.join(s_1), "\n" +
+          '  '.join(s_2), "\n" +
+          '  '.join(s_3), "\n" +
+          "-------------")
     print(wins())
 
 
+def step():
+    cycle = 0
+    global s_1, s_2, s_3, answer_1, answer_2
+    while cycle != 1:
+        print("Введите координаты:")
+        try:    # ключевое слово для обработки исключений
+            answer_1, answer_2 = input().split(' ')  # split разбивает строки на подстроки в зависимости от разделителя
+            answer_1 = int(answer_1)
+            answer_2 = int(answer_2)
+        except ValueError:  # исключение ошибки
+            print("Вы должны ввести числа!")
+            continue
+        if answer_1 and answer_2 not in dig:
+            print("Координаты должны быть от 1 до 3!")
+        elif answer_1 and answer_2 in dig:
+            if s_l[answer_1][answer_2] != '_':  # для определения занятости клетки под её координатой
+                print("Эта клетка занята! Выбери другую!")
+            else:
+                s_l[int(answer_1)][int(answer_2)] = "X"
+                cycle = 1
+    square()
+
+
 square()
+step()
