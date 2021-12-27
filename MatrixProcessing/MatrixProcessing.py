d1ef3e0c470ac582c743b5bddf95ec4d9d0426f1
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-27 19:37:45 +0200

    MatrixProcessing 1-nd stage

---
+++ MatrixProcessing/MatrixProcessing.py
@@ -0,0 +1,36 @@
+str_m1 = str_m2 = stl_m1 = stl_m2 = 0
+e = []
+b = c = d = []
+
+
+def mat():
+    global str_m1, str_m2, stl_m1, stl_m2, b, c
+    str_m1, stl_m1 = input("Введите количество строк и столбцов 1-ой матрицы:").split(" ")
+    b = [[int(k) for k in input().split(" ")] for t in range(int(str_m1))]
+    str_m2, stl_m2 = input("Введите количество строк и столбцов 2-ой матрицы:").split(" ")
+    c = [[int(k) for k in input().split(" ")] for t in range(int(str_m2))]
+
+
+def res():
+    for i in range(int(str_m1)):
+        print(" ".join(map(str, d[i])))
+
+
+def summa():
+    global d, e
+    mat()
+    if str_m1 == str_m2:
+        if stl_m1 == stl_m2:
+            for i in range(int(str_m1)):
+                for j in range(int(stl_m1)):
+                    e.append(b[i][j] + c[i][j])
+                d.append(e)
+                e = []
+            res()
+        else:
+            print("ERROR")
+    else:
+        print("ERROR")
+
+
+summa()
