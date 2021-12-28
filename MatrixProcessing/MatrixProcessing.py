1d94b43690f40d348867926d1898b8ff8a032a4b
dimon4ig <d.o.sadovsjkyy@student.khai.edu>
2021-12-28 03:24:06 +0200

    MatrixProcessing 6-th stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,6 +1,6 @@
 from math import ceil
 
-str_m1 = str_m2 = stl_m1 = stl_m2 = s = 0
+str_m1 = str_m2 = stl_m1 = stl_m2 = s = con = 0
 e = []
 b = c = d = []
 answer = 100
@@ -39,17 +39,14 @@ def res():
     d = []
 
 
-def constant():
-    global str_m1, stl_m1, b, e, d
-    str_m1, stl_m1 = input("Введите количество строк и столбцов матрицы: ").split(" ")
-    b = [[int(k) for k in input().split(" ")] for t in range(int(str_m1))]
-    cons = int(input("Введите константу: "))
-    for i in range(int(str_m1)):
-        for j in range(int(stl_m1)):
-            e.append(b[i][j] * cons)
+def constant(bk):
+    global e, d, con
+    for i in range(str_m1):
+        for j in range(stl_m1):
+            e.append(round(bk[i][j] * con, 2))
         d.append(e)
         e = []
-    res()
+    return d
 
 
 def mat_x():
@@ -69,23 +66,33 @@ def mat_x():
         print("Операция не может быть выполнена.")
 
 
+def matrix():
+    global str_m1, stl_m1, b
+    str_m1, stl_m1 = input("Введите количество строк и столбцов матрицы:").split(" ")
+    print("Введите матрицу:")
+    b = [[int(k) for k in input().split(" ")] for _ in range(int(str_m1))]
+
+
 def res_s():
     global b
     print("Результат:")
     for i in range(int(str_m1)):
-        print(' '.join(map(str, (b[i]))))
+        print(" ".join(map(str, (b[i]))))
     b = []
 
 
 def t_mat():
-    global str_m1, stl_m1, b, e, d
-    str_m1, stl_m1 = input("Введите количество строк и столбцов матрицы:").split(" ")
-    b = [[int(k) for k in input().split(" ")] for _ in range(int(str_m1))]
+    global str_m1, stl_m1, b
+    str_m1, stl_m1 = input("Введите количество строк и столбцов матрицы: ").split(" ")
+    str_m1 = int(str_m1)
+    stl_m1 = int(stl_m1)
+    print("Введите матрицу:")
+    b = [[int(k) for k in input().split(" ")] for _ in range(str_m1)]
 
 
 def t_mat_side():
     global str_m1, stl_m1, b
-    t_mat()
+    matrix()
     l = int(str_m1) - 1
     for i in range(l + 1):
         for j in range(l + 1):
@@ -96,7 +103,7 @@ def t_mat_side():
 
 def t_mat_main():
     global str_m1, stl_m1, b
-    t_mat()
+    matrix()
     l = int(str_m1)
     for i in range(l):
         for j in range(l):
@@ -107,7 +114,7 @@ def t_mat_main():
 
 def t_mat_ver():
     global str_m1, stl_m1, b
-    t_mat()
+    matrix()
     l = int(str_m1) - 1
     f = ceil(l / 2)
     for i in range(l + 1):
@@ -118,7 +125,7 @@ def t_mat_ver():
 
 def t_mat_hor():
     global str_m1, stl_m1, b
-    t_mat()
+    matrix()
     lo = int(str_m1) - 1
     f = ceil(lo / 2)
     for i in range(f):
@@ -131,7 +138,7 @@ def t_mat_info():
     print("1.Главная диагональ\n"
           "2. Боковая диагональ\n"
           "3. Вертикальная линия\n"
-          "4. Горизонтальная линия\n")
+          "4. Горизонтальная линия")
     choice = int(input("Ваш выбор: "))
     if choice == 1:
         t_mat_main()
@@ -144,8 +151,10 @@ def t_mat_info():
     else:
         print("Операция не может быть выполнена.")
 
+
 def minor(h, i, u):
     return [k[:u] + k[u + 1:] for k in (h[:i] + h[i + 1:])]
+
 
 def determ(l):
     if len(l) == 2:
@@ -156,12 +165,39 @@ def determ(l):
     return det
 
 
+def inwerse_mat_main(bk):
+    global str_m1, stl_m1
+    for i in range(str_m1):
+        for j in range(str_m1):
+            if i > j:
+                bk[i][j], bk[j][i] = bk[j][i], bk[i][j]
+    return bk
+
+
+def inwerse_m(k):
+    global con, d
+    for i in range(str_m1):
+        for j in range(stl_m1):
+            if i + j % 2 == 0:
+                b_1[i][j] = determ(minor(k, i, j))
+            else:
+                b_1[i][j] = -(determ(minor(k, i, j)))
+    try:
+        con = 1 / determ(k)
+        cad = constant(inwerse_mat_main(b_1))
+        for z in range(str_m1):
+            print(' '.join(map(str, cad[z])))
+    except ZeroDivisionError:
+        print("Эта матрица не имеет инверсии.")
+
+
 while answer != 0:
     answer = int(input("1. Сложить матрицы\n"
                        "2. Умножить матрицу на константу\n"
                        "3. Умножить матрицы\n"
                        "4. Матрица транспонирования\n"
                        "5. Вычислить определитель\n"
+                       "6. Обратная матрица\n"
                        "0. Выход\n"
                        "Ваш выбор:"))
     if answer == 0:
@@ -170,7 +206,15 @@ while answer != 0:
     elif answer == 1:
         summa()
     elif answer == 2:
-        constant()
+        str_m1, stl_m1 = input("Введите количество строк и столбцов матрицы: ").split(" ")
+        b = [[float(k) for k in input().split(" ")] for _ in range(int(str_m1))]
+        con = float(input("Введите константу: "))
+        for i in range(int(str_m1)):
+            for j in range(int(stl_m1)):
+                e.append(b[i][j] * con)
+            d.append(e)
+            e = []
+        res()
     elif answer == 3:
         mat_x()
     elif answer == 4:
@@ -178,3 +222,9 @@ while answer != 0:
     elif answer == 5:
         t_mat()
         print("Результат: " + str(determ(b)))
+    elif answer == 6:
+        t_mat()
+        b_1 = [["_" for k in range(str_m1)] for v in range(stl_m1)]
+        print("Результат:")
+        inwerse_m(b)
+        print()
